4e0f7981c38c3e068eb99942d315a9508efc235e
Nikolaj <abonner.et@gmail.com>
2022-10-03 21:37:53 +0200

    More responsive layout

--- public/js/tech.js
+++ public/js/tech.js
@@ -1,8 +1,59 @@
 
-const techs = {
-    javascript: {
-        title: "Javascript",
-        description: "JavaScript",
-        background: "#fff"
-    }
+const techs = [{
+    title: "Investment Calculator",
+    description: "JavaScript",
+    background: "black"
+},{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+},{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
 }
+,{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+},{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+},{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+}
+,{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+},{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+},{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+}
+,{
+    title: "Javascript",
+    description: "JavaScript",
+    background: "black"
+}]
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < techs.length; i++) {
+        document.getElementById("tech_folder").innerHTML = document.getElementById("tech_folder").innerHTML + `
+        <div class="tech">
+            <div>
+                <h2>${techs[i].title}</h2>
+                <p>${techs[i].description}</p>
+            </div>
+        </div>
+        `
+    }
+})
